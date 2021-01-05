5ab5b750e5f59cf11b2d5c37a041c28bcc9f1049
Layerex <layerex@dismail.de>
2021-01-05 17:41:43 +0300

    Remove redundant import

--- bandcamp_name_your_price_dl.py
+++ bandcamp_name_your_price_dl.py
@@ -15,7 +15,6 @@ import requests
 from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
 
